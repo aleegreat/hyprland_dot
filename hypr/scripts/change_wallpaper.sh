4548240904adf0b79f89df8c26b1e30c15446272
aleegreat <aleegreat@gmail.com>
2023-02-18 16:50:53 +0800

    Update change_wallpaper.sh

--- hypr/scripts/change_wallpaper.sh
+++ hypr/scripts/change_wallpaper.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
+echo header
+
 export XDG_CACHE_HOME=~/.cache
 
 WALLPAPER_PATH=~/Pictures/wallpapers
 IMG_NAME=$(ls ${WALLPAPER_PATH} | shuf -n 1)
 IMG_PATH=${WALLPAPER_PATH}/$IMG_NAME
-
 #swaybg -i "${IMG_PATH}"
 
 init(){
-    swww init
-    swww img "${IMG_PATH}" --transition-step 20 --transition-fps 60 --transition-type random
+    /usr/bin/swww init
+    sleep 3
+    swww img "${IMG_PATH}" --transition-step 100 --transition-fps 60 --transition-type random
 }
 change(){
-    swww img "${IMG_PATH}" --transition-step 20 --transition-fps 60 --transition-type random
+    swww img "${IMG_PATH}" --transition-step 100 --transition-fps 60 --transition-type random ;
 }
 
 case "$1" in
