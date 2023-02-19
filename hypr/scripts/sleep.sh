758ab53396e9592acee9026326661ecc7a76b6eb
aleegreat <aleegreat@gmail.com>
2023-02-19 17:47:15 +0800

    Update exec_once.conf, sleep.sh, and 3 more files...

--- hypr/scripts/sleep.sh
+++ hypr/scripts/sleep.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-swayidle -w \
-        before-sleep swaylock \
-        timeout 180 'temp=$(brightnessctl g); brightnessctl set $((temp / 2))' \
+swayidle timeout 60 'temp=$(brightnessctl g); brightnessctl set $((temp / 2))' \
             resume 'temp=$(brightnessctl g); brightnessctl set $((temp * 2))' \
-        timeout 600 "hyprctl dispatch dpms off" \
+         timeout 180 'swaylock -f' \
+        timeout 300 "hyprctl dispatch dpms off" \
             resume 'hyprctl dispatch dpms on' \
-        timeout 600 'systemctl suspend' \
+        timeout 1800 'systemctl suspend' \
             resume 'hyprctl dispatch dpms on'
