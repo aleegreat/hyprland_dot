d4247706e500e0d1a79bfd83712e8ed8cf29c13a
aleegreat <aleegreat@gmail.com>
2023-02-27 12:28:44 +0800

    update

--- hypr/scripts/sleep.sh
+++ hypr/scripts/sleep.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
+
 status=`acpi -a | awk -F: '{printf $2}'`
 
 # Adapter 0: on-line
-if [[ $status == 'on-line' ]]; then
+if [[ $status == "on-line" ]]; then
     bright_time=300
     lock_time=300
     dpms_time=3600
@@ -15,10 +16,17 @@ else
     dpms_time=600
     suspend_time=1800
 fi
-swayidle timeout $bright_time 'temp=$(brightnessctl g); brightnessctl set $((temp / 2))' \
-            resume 'temp=$(brightnessctl g); brightnessctl set $((temp * 2))' \
-         timeout $lock_time 'swaylock -f' \
+
+temp=$(brightnessctl g);
+if [[ $temp -eq 0 ]]; 
+then
+    temp=100
+fi
+swayidle -w before-sleep "swaylock -f" \
+        timeout $bright_time "brightnessctl set $((temp/2))" \
+            resume "brightnessctl set $temp" \
+        timeout $lock_time "swaylock -f" \
         timeout $dpms_time "hyprctl dispatch dpms off" \
-            resume 'hyprctl dispatch dpms on' \
-        timeout $suspend_time 'systemctl suspend' \
-            resume 'hyprctl dispatch dpms on'
+            resume "hyprctl dispatch dpms on" \
+        timeout $suspend_time "systemctl suspend" \
+            resume "hyprctl dispatch dpms on"
